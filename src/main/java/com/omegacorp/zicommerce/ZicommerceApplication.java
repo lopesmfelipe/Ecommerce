4a23fd791a9643473d983d7032d5bc6e4d3681df
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-11-22 11:35:42 -0400

    Created project

---
+++ src/main/java/com/omegacorp/zicommerce/ZicommerceApplication.java
@@ -0,0 +1,13 @@
+package com.omegacorp.zicommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZicommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZicommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omegacorp/zicommerce/ZicommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omegacorp.zicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZicommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
